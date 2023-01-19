59f06faec451610cc39db5631b296e1924217ea2
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-19 23:21:27 +0530

    Update and rename WelcomeToAddressBookSystem.java to AddressBookMain.java

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.addressbooksystem;
 
-public class WelcomeToAddressBookSystem {
+public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
     }
