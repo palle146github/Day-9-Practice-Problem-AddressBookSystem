4203dac81840e7be90867581a6909f905763aa29
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-20 13:49:34 +0530

    Update Contact.java

--- src/com/bridgelabz/addressbooksystem/Contact.java
+++ src/com/bridgelabz/addressbooksystem/Contact.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.addressbooksystem;
-
+// Contact class with the imformation as a variable which are added to the Address Book
 public class Contact {
     private String firstname, lastname, address, city, state, zip, phonenumber ,email;
-    Contact(){}
+    Contact(){}// default constructor declaration
 
+    //parameterized constructor for initializing the objects with the current objects
     public Contact(String firstname, String lastname, String address, String city, String state, String zip, String phonenumber, String email) {
         this.firstname = firstname;
         this.lastname = lastname;
@@ -12,6 +13,71 @@ public class Contact {
         this.state = state;
         this.zip = zip;
         this.phonenumber = phonenumber;
+        this.email = email;
+    }
+
+    // getters and setters
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
