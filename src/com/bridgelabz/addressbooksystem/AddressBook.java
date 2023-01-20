9e6e5178a202473538b3c3d1ab9c067130ece439
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-20 13:57:14 +0530

    Update AddressBook.java

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -3,26 +3,34 @@ package com.bridgelabz.addressbooksystem;
 import java.util.Scanner;
 
 public class AddressBook {
-    Contact contact = new Contact();
-
-
+    
+    Contact contact = new Contact();// Contact object declaration
+    
+    // method for adding a contact into address book system
     public void addContact() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter contact details");
         System.out.println("Enter first name");
         String firstname = scanner.next();
+        
         System.out.println("Enter last name");
         String lastname = scanner.next();
+        
         System.out.println("Enter Address");
         String address = scanner.next();
+        
         System.out.println("Enter City");
         String city = scanner.next();
+        
         System.out.println("Enter State");
         String state = scanner.next();
+        
         System.out.println("Enter Pincode");
         String zip = scanner.next();
+        
         System.out.println("Enter Phone number");
         String phonenumber = scanner.next();
+        
         System.out.println("Enter Email");
         String email = scanner.next();
 
